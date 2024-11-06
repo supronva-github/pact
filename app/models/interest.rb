d53ba7b833b170d360f9d5f6284ee76f94084c4c
Vitaliy Supron <supronva@gmail.com>
2024-11-06 08:33:35 +0300

    add alternative association

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Interest < ApplicationRecord
-  has_and_belongs_to_many :users
+  # has_and_belongs_to_many :users <- вариант если не нужна моделька
+  has_many :interests_users
+  has_many :users, through: :interests_users
 end
--- app/models/skil.rb
+++ app/models/skil.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Skil < ApplicationRecord
-  has_and_belongs_to_many :users
+  # has_and_belongs_to_many :users  <- вариант если не нужна моделька
+  has_many :skils_users
+  has_many :users, through: :skils_users
 end
